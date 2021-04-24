dd0f35f33e6fca9a1e96bfc9f88b89ec978baee7
Ramrin <iwgmw.nir02@gmail.com>
2021-04-24 21:47:11 +0900

    リストのタイトル保存処理

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,10 +1,53 @@
 class ListsController < ApplicationController
+  # topページ(リスト一覧表示ページ)
   def index
-    # @lists = List.all
+    @lists = List.all
   end
 
+  # リスト新規作成ページ
   def new
-    # @list = List.new()
+    @list = List.new
+  end
+
+  # newアクションで作成したデータを保存
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to root_path
+  end
+
+  # リスト詳細ページ
+  # def show
+  #   @list = List.find(list_params)
+  # end
+
+  # リスト編集ページ
+  # def edit
+  #   @list = List.find(params[:id])
+  # end
+
+  # @listに変数がある場合の編集処理
+  # def update
+  #   @list = list.find(params[:id])
+  #   if @list.update(list_params)
+  #     redirect_to list_path
+  #   else
+  #     redirect_toedit_list_path
+  #   end
+  # end
+
+  # リストを削除する処理
+  # def destroy
+  #   @list = List.find(params[:id])
+  #   @list.destroy
+  #   redirect_to list_path
+  # end
+
+  private
+
+  # リストの情報の保存を許可する
+  def list_params
+    params.require(:list).permit(:title)
   end
 
 end
